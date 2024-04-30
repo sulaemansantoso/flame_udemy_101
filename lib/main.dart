425b8b7fd5675388e7180f736e2130db56352e3e
Sulaeman.santoso.mac <sulaeman.santoso@gmail.com>
2024-04-30 10:34:16 +0700

    adding more comments

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ class MyGameWidget extends StatelessWidget {
             return StartMenuOverlay(game);
           },
         },
+        //adding initial overlays
         initialActiveOverlays: const ["startmenu"],
       ),
     );
